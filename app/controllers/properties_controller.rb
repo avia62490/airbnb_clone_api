c0bc2f2066c2a22d2fbe24eaaa21d39b6bc2ca0a
Alex Via <alexandervia@gmail.com>
2023-03-02 19:07:17 -0600

    adds dynamic property udeate function and route

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -24,4 +24,23 @@ class PropertiesController < ApplicationController
     render json: property.as_json
   end
 
+  def update
+    property = Property.find(params[:id])
+    property.title = params[:title] || property.title
+    property.address = params[:address] || property.address
+    property.description = params[:description] || property.description
+    property.price_rate = params[:price_rate] || property.price_rate
+    property.max_guests = params[:max_guests] || property.max_guests
+    property.bedrooms = params[:bedrooms] || property.bedrooms
+    property.bathrooms = params[:bathrooms] || property.bathrooms
+    property.service_fee = params[:service_fee] || property.service_fee
+    property.type = params[:type] || property.type
+    property.hot_tub = params[:hot_tub] || property.hot_tub
+    property.pool = params[:pool] || property.pool
+    property.laundry = params[:laundry] || property.laundry
+    property.fireplace = params[:fireplace] || property.fireplace
+    property.save
+    render json: property.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  ####### HOUSE ##########
+  ####### PROPERTY ##########
   get "/properties" => "properties#index"
   post "/properties" => "properties#create"
   get "/properties/:id" => "properties#show"
+  patch "/properties/:id" => "properties#update"
 end
