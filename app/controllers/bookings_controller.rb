cad0772b907d4b0579ea8a8399eb24dca93d8c72
Alex Via <alexandervia@gmail.com>
2023-03-16 15:57:04 -0500

    update function works correctly and also checks for overlapping boookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,9 +31,32 @@ class BookingsController < ApplicationController
   end
 
   def update
+
     @booking = Booking.find_by(id: params[:id])
-    # render json: @booking.date_range.as_json
-    render json: @booking.calculate_sub_total.as_json
+    @booking.check_in_date = params[:check_in_date] || @booking.check_in_date
+    @booking.check_out_date = params[:check_out_date] || @booking.check_out_date
+
+    @booking.sub_total = @booking.calculate_sub_total
+    @booking.service_fee = @booking.calculate_service_fee
+    @booking.total = @booking.calculate_total
+
+    overlapping_bookings = Booking.where(
+      property_id: @booking.property_id
+    ).where(
+      "id != ? AND
+      check_in_date < ? AND
+      check_out_date > ?",
+      @booking.id,
+      @booking.check_out_date,
+      @booking.check_in_date
+    )
+
+    if overlapping_bookings.any?
+      render json: {message: "Date range conflicts with a current booking for this property"}
+    else
+      @booking.save
+      render template: "bookings/show"
+    end
 
   end
 
